d239285df2d86b9754a4d8eed24d721e6a366655
andypugh <andy@bodgesoc.org>
2021-03-28 22:13:03 +0100

    HAL: Add the Beaglebone PRU driver.
    Signed-off-by: andypugh <andy@bodgesoc.org>

---
+++ src/hal/drivers/hal_pru_generic/pru.h
@@ -0,0 +1,5 @@
+XSym
+0023
+a1d3e6cebc878b0e8a476fa4aae25e5c
+../../support/pru/pru.h
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
