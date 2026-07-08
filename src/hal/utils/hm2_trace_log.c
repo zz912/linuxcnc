0d20eb2011ce237fefabac48b53a1e301205d3ae
zz912 <zz912@seznam.cz>
2026-07-08 21:28:28 +0200

    hostmot2: add userspace trace logger

--- src/hal/utils/hm2_trace_log.c
+++ src/hal/utils/hm2_trace_log.c
@@ -1,10 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 
 #include "rtapi.h"
 #include "hal.h"
 
 #include "../drivers/mesa-hostmot2/hm2_trace_export.h"
+
+static FILE *open_log_file(void)
+{
+    char path[512];
+    char timestamp[64];
+    time_t now;
+    struct tm tm;
+    const char *home;
+
+    home = getenv("HOME");
+    if (home == NULL)
+        return NULL;
+
+    now = time(NULL);
+    localtime_r(&now, &tm);
+
+    strftime(
+        timestamp,
+        sizeof(timestamp),
+        "%Y_%m_%d-%H_%M_%S",
+        &tm);
+
+    snprintf(
+        path,
+        sizeof(path),
+        "%s/hm2_trace_%s.log",
+        home,
+        timestamp);
+
+    return fopen(path, "w");
+}
 
 int main(void)
 {
@@ -40,14 +73,32 @@ int main(void)
 
     hal_ready(comp_id);
 
-    printf("HM2 trace logger attached.\n");
-    printf("version    = %u\n", shmem->version);
-    printf("valid      = %u\n", shmem->valid);
-    printf("generation = %u\n", shmem->generation);
-    printf("ring_size  = %u\n", shmem->ring_size);
-    printf("ring_head  = %u\n", shmem->ring_head);
+    for (;;) {
 
-    getchar();
+        if (!shmem->valid) {
+            usleep(100000);
+            continue;
+        }
+
+        FILE *log;
+
+        log = open_log_file();
+        if (log == NULL) {
+            fprintf(stderr, "cannot create log file\n");
+            break;
+        }
+
+        fprintf(log, "HM2 TRACE\n\n");
+        fprintf(log, "generation : %u\n", shmem->generation);
+        fprintf(log, "ring_head  : %u\n", shmem->ring_head);
+        fprintf(log, "total_tmax : %u ns\n", shmem->total_tmax_ns);
+
+        fclose(log);
+
+        printf("Trace written.\n");
+
+        break;
+    }
 
     hal_exit(comp_id);
 
